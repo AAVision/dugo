e0be48187a54826e52749508b58684b96de99d68
knbr13 <abdullahalaadine63@gmail.com>
2025-03-06 00:32:43 +0200

    create compareByteByByte func

--- process.go
+++ process.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"bytes"
+	"io"
+	"os"
+)
+
 func groupByHash(files []string) ([]string, error) {
 	m := map[string][]string{}
 
@@ -20,4 +26,28 @@ func groupByHash(files []string) ([]string, error) {
 	}
 
 	return s, nil
+}
+
+func compareByteByByte(file1, file2 string) (b bool, err error) {
+	f1, err := os.Open(file1)
+	if err != nil {
+		return b, err
+	}
+	defer f1.Close()
+
+	f2, err := os.Open(file2)
+	if err != nil {
+		return b, err
+	}
+	defer f2.Close()
+
+	d1, err := io.ReadAll(f1)
+	if err != nil {
+		return b, err
+	}
+	d2, err := io.ReadAll(f2)
+	if err != nil {
+		return b, err
+	}
+	return bytes.Equal(d1, d2), nil
 }
